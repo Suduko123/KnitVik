100f4ac4d9406da8557e1b1142b204fb099729b8
Christoffer Rod Greffel <33146387+Suduko123@users.noreply.github.com>
2023-04-14 09:33:01 +0200

    Added sections and videoes
    Created basic strucure for different sections and added videoes.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// ZOOM EFFECT
 const zoomContainer = document.getElementById("zoom-container");
 const zoomImage = document.getElementById("zoom-image");
 const zoomImage2 = document.querySelector("#zoom-image2 svg");
-const content = document.getElementById("welcome");
+const content = document.getElementById("content");
 
 let backgroundScale = 1;
 let imageScale = 1.6;
@@ -34,7 +35,8 @@ window.addEventListener("wheel", function (e) {
 
   if (backgroundScale >= 4 && !backgroundFullyRevealed) {
     backgroundFullyRevealed = true;
-    content.style.display = "flex";
+    content.style.display = "block";
+    init();
   } else if (backgroundScale < 4 && backgroundFullyRevealed) {
     backgroundFullyRevealed = false;
     content.style.display = "none";
@@ -46,3 +48,43 @@ function isZoomContainerFullyInView() {
   const viewportHeight = window.innerHeight;
   return zoomContainerRect.bottom === viewportHeight;
 }
+
+// TIMELINE (Horizontal scroll)
+
+// Sets the height of the sticky container element based on the width of the horizontal container element nested within it.
+function setStickyContainersSize() {
+  const container = document.querySelector("#sticky-container");
+  const stikyContainerHeight = container.querySelector("#horizontal-container").scrollWidth;
+  container.style.height = `${stikyContainerHeight}px`;
+}
+
+// Checks if a given element is currently in the viewport.
+const isElementInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
+};
+
+// Handles the scrolling behavior for the sticky container element when the user scrolls with the mouse wheel.
+// It determines if the container is in the viewport, and if it is, it checks if it can scroll horizontally and updates the
+// horizontal scroll position based on the mouse wheel delta.
+function wheelHandler(evt) {
+  const stickyContainer = document.querySelector("#sticky-container");
+
+  if (!isElementInViewport(stickyContainer)) {
+    return;
+  }
+
+  const isContainerAboveViewport = stickyContainer.offsetTop < document.documentElement.scrollTop;
+  const isContainerBelowViewport =
+    stickyContainer.offsetTop + stickyContainer.offsetHeight > document.documentElement.scrollTop;
+  let canScrollHorizontally = isContainerAboveViewport && isContainerBelowViewport;
+
+  if (canScrollHorizontally) {
+    stickyContainer.querySelector("#horizontal-container").scrollLeft += evt.deltaY;
+  }
+}
+
+const init = () => {
+  setStickyContainersSize();
+  window.addEventListener("wheel", wheelHandler);
+};
