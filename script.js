bbb143c6313ccf07abaa4a8eb97e1984abbc48f8
catrinekjaergaard <114469237+catrinekjaergaard@users.noreply.github.com>
2023-04-18 13:40:17 +0200

    Revert "Responsive design"
    This reverts commit cc514a9b846b4d98a28a020b2b7e24f41f53116a.

--- script.js
+++ script.js
@@ -9,47 +9,46 @@ let imageScale = 1.6;
 const startingScale = 1;
 let backgroundFullyRevealed = false;
 
-if (window.innerWidth > 500) {
-  window.addEventListener("wheel", function (e) {
-    if (e.deltaY > 0) {
-      // Zoom in
-      if (backgroundScale < 4) {
-        backgroundScale += 0.5;
-        imageScale -= 0.1;
+window.addEventListener("wheel", function (e) {
+  if (e.deltaY > 0) {
+    // Zoom in
+    if (backgroundScale < 4) {
+      backgroundScale += 0.5;
+      imageScale -= 0.1;
+    }
+  } else {
+    // Zoom out
+    if (backgroundScale > startingScale) {
+      if (isZoomContainerFullyInView()) {
+        backgroundScale -= 0.5;
+        imageScale += 0.1;
       }
     } else {
-      // Zoom out
-      if (backgroundScale > startingScale) {
-        if (isZoomContainerFullyInView()) {
-          backgroundScale -= 0.5;
-          imageScale += 0.1;
-        }
-      } else {
-        backgroundScale = startingScale;
-        imageScale = 1.6;
-        backgroundFullyRevealed = false;
-      }
-    }
-
-    zoomImage.style.transform = `scale(${backgroundScale})`;
-    zoomImage2.style.transform = `scale(${imageScale})`;
-
-    if (backgroundScale >= 4 && !backgroundFullyRevealed) {
-      backgroundFullyRevealed = true;
-      content.style.display = "block";
-      init();
-    } else if (backgroundScale < 4 && backgroundFullyRevealed) {
+      backgroundScale = startingScale;
+      imageScale = 1.6;
       backgroundFullyRevealed = false;
-      content.style.display = "none";
     }
-  });
-}
+  }
+
+  zoomImage.style.transform = `scale(${backgroundScale})`;
+  zoomImage2.style.transform = `scale(${imageScale})`;
+
+  if (backgroundScale >= 4 && !backgroundFullyRevealed) {
+    backgroundFullyRevealed = true;
+    content.style.display = "block";
+    init();
+  } else if (backgroundScale < 4 && backgroundFullyRevealed) {
+    backgroundFullyRevealed = false;
+    content.style.display = "none";
+  }
+});
 
 function isZoomContainerFullyInView() {
   const zoomContainerRect = zoomContainer.getBoundingClientRect();
   const viewportHeight = window.innerHeight;
   return zoomContainerRect.bottom === viewportHeight;
 }
+
 // TIMELINE (Horizontal scroll)
 
 // Sets the height of the sticky container element based on the width of the horizontal container element nested within it.
